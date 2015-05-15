86bd5c26f3db84e114a236b39629c881c52b8e46
Romaric FAVE <romaric.fave@epitech.eu>
2015-05-15 16:48:17 +0200

    droite gauche

--- src/client/my_droite.c
+++ src/client/my_droite.c
@@ -1,19 +1,20 @@
 /*
 ** my_droite.c for my_droite in /home/lopez_t/Zappy/src/client
-** 
+**
 ** Made by Thibaut Lopez
 ** Login   <lopez_t@epitech.net>
-** 
+**
 ** Started on  Tue May 12 14:56:11 2015 Thibaut Lopez
-** Last update Tue May 12 19:05:37 2015 Thibaut Lopez
+** Last update Fri May 15 16:48:06 2015 romaric
 */
 
 #include "server.h"
 
-int	my_droite(char **com, t_zap *data, t_user *usr)
+int	my_droite(__attribute__((unused)) char **com
+		  , __attribute((unused)) t_zap *data
+		  , t_user *usr)
 {
-  (void)com;
-  (void)data;
-  (void)usr;
+  GET_DIR(usr) = S_MOD(GET_DIR(usr) + 1, 4);
+  fill_cb(&usr->wr, "ok\n", 3);
   return (0);
 }
--- src/client/my_gauche.c
+++ src/client/my_gauche.c
@@ -1,19 +1,20 @@
 /*
 ** my_gauche.c for my_gauche in /home/lopez_t/Zappy/src/client
-** 
+**
 ** Made by Thibaut Lopez
 ** Login   <lopez_t@epitech.net>
-** 
+**
 ** Started on  Tue May 12 14:56:11 2015 Thibaut Lopez
-** Last update Tue May 12 19:06:58 2015 Thibaut Lopez
+** Last update Fri May 15 16:47:27 2015 romaric
 */
 
 #include "server.h"
 
-int	my_gauche(char **com, t_zap *data, t_user *usr)
+int	my_gauche(__attribute__((unused)) char **com
+		  , __attribute__((unused)) t_zap *data
+		  , t_user *usr)
 {
-  (void)com;
-  (void)data;
-  (void)usr;
+  GET_DIR(usr) = S_MOD(GET_DIR(usr) - 1, 4);
+  fill_cb(&usr->wr, "ok\n", 3);
   return (0);
 }

1c9291d4a4c0fbb377bbbac2865286d3857aa579
Romaric FAVE <romaric.fave@epitech.eu>
2015-05-15 15:55:21 +0200

    advance

--- src/client/my_avance.c
+++ src/client/my_avance.c
@@ -1,19 +1,25 @@
 /*
 ** my_avance.c for my_avance in /home/lopez_t/Zappy/src/client
-** 
+**
 ** Made by Thibaut Lopez
 ** Login   <lopez_t@epitech.net>
-** 
+**
 ** Started on  Tue May 12 14:56:11 2015 Thibaut Lopez
-** Last update Tue May 12 19:04:51 2015 Thibaut Lopez
+** Last update Fri May 15 15:51:00 2015 romaric
 */
 
 #include "server.h"
 
-int	my_avance(char **com, t_zap *data, t_user *usr)
+int	my_avance(__attribute__((unused))char **com,
+		  t_zap *data, t_user *usr)
 {
-  (void)com;
-  (void)data;
-  (void)usr;
+  t_pair	pos;
+
+  pos.f = GET_X(usr);
+  pos.s = GET_Y(usr);
+  gofo[GET_DIR(usr)](&pos, 1);
+  GET_X(usr) = S_MOD(pos.f, data->length);
+  GET_Y(usr) = S_MOD(pos.s, data->width);
+  fill_cb(&usr->wr, "ok\n", 3);
   return (0);
 }

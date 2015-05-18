2d496c4ae680acee41550f328c74b597da5149ed
lopez_t <thibaut.lopez@epitech.eu>
2015-05-18 17:35:44 +0200

    fixing some bugs

--- src/graphic/my_pnw.c
+++ src/graphic/my_pnw.c
@@ -5,7 +5,7 @@
 ** Login   <lopez_t@epitech.net>
 ** 
 ** Started on  Thu May 14 01:07:32 2015 Thibaut Lopez
-** Last update Thu May 14 19:14:04 2015 Thibaut Lopez
+** Last update Mon May 18 17:33:41 2015 Thibaut Lopez
 */
 
 #include "server.h"
@@ -24,9 +24,11 @@ int		my_pnw(t_user *usr, t_user *player)
 	nbr++;
       tmp = tmp->prev;
     }
+  if (tmp->type == AI && tmp != player)
+    nbr++;
   bzero(str, 200);
   sprintf(str, "pnw #%d %d %d %d %d %s\n",
-	  nbr, GET_X(player), GET_Y(player), GET_DIR(player),
+	  nbr, GET_X(player), GET_Y(player), GET_DIR(player) + 1,
 	  GET_LVL(player), GET_TEAM(player));
   fill_cb(&usr->wr, str, strlen(str));
   return (0);

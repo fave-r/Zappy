6a2e12e0c6e0a4e9943d1dd4e61b33354d79f74d
Romaric FAVE <romaric.fave@epitech.eu>
2015-05-22 13:39:47 +0200

    func_lev

---
+++ src/client/func_level.c
@@ -0,0 +1,29 @@
+/*
+** func_level.c for zappy in /home/fave_r/rendu/Zappy/src/client
+**
+** Made by romaric
+** Login   <fave_r@epitech.net>
+**
+** Started on  Fri May 22 13:37:47 2015 romaric
+** Last update Fri May 22 13:39:20 2015 romaric
+*/
+
+int	level_4(t_content *cont, t_user *usr)
+{
+
+}
+
+int	level_5(t_content *cont, t_user *usr)
+{
+
+}
+
+int	level_6(t_content *cont, t_user *usr)
+{
+
+}
+
+int	level_7(t_content *cont, t_user *usr)
+{
+
+}

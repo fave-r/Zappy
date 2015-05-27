916dadfb4f74b813917db508659df3bb56821faf
lopez_t <thibaut.lopez@epitech.eu>
2015-05-27 18:35:00 +0200

    fork complete

--- src/client/my_other.c
+++ src/client/my_other.c
@@ -5,7 +5,7 @@
 ** Login   <lopez_t@epitech.net>
 ** 
 ** Started on  Tue May 12 14:56:11 2015 Thibaut Lopez
-** Last update Tue May 26 18:14:10 2015 Thibaut Lopez
+** Last update Wed May 27 18:28:34 2015 Thibaut Lopez
 */
 
 #include "server.h"
@@ -22,14 +22,14 @@ void		send_client_info(t_team *team, t_zap *data, t_user *usr)
   fill_cb(&usr->wr, tmp, strlen(tmp));
 }
 
-t_plr		*player_info(t_team *team, int length, int width)
+t_plr		*player_info(t_team *team, t_pair *pos)
 {
   t_plr		*plr;
 
   plr = xmalloc(sizeof(t_plr));
   plr->team = team;
-  plr->x = rand() % length;
-  plr->y = rand() % width;
+  plr->x = pos->f;
+  plr->y = pos->s;
   plr->dir = rand() % 4;
   plr->inv.food = 10;
   plr->inv.linemate = 0;
@@ -45,18 +45,40 @@ t_plr		*player_info(t_team *team, int length, int width)
   return (plr);
 }
 
+int	hatching_egg(t_pair *pos, t_user *usr, t_zap *data, t_team *cur)
+{
+  t_tv	now;
+  t_egg	*frt;
+
+  frt = front_q(cur->eggs);
+  gettimeofday(&now, NULL);
+  if (frt == NULL || data->count > count_in_team(cur, usr))
+    {
+      pos->f = rand() % data->length;
+      pos->s = rand() % data->width;
+      return (0);
+    }
+  pos->f = frt->pos.f;
+  pos->s = frt->pos.s;
+  push_q(&usr->queue, &frt->hatch, clone_tv);
+  pop_q(&cur->eggs);
+  return (0);
+}
+
 int		my_other(char **com, t_zap *data, t_user *usr)
 {
   t_team	*cur;
   char		str[256];
   t_user	*tmp;
+  t_pair	pos;
 
   cur = data->teams;
   while (cur != NULL && strcmp(cur->name, com[0]) != 0)
     cur = cur->next;
-  if (cur == NULL || count_in_team(cur, usr) == data->count)
+  if (cur == NULL || count_in_team(cur, usr) == cur->count)
     return (-1);
-  usr->plr = player_info(cur, data->length, data->width);
+  hatching_egg(&pos, usr, data, cur);
+  usr->plr = player_info(cur, &pos);
   tmp = usr;
   while (tmp != NULL && tmp->prev != NULL)
     tmp = tmp->prev;

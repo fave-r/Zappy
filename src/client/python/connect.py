e8a60a765a57b308c415487af616f1a8fa12ce51
Arnaud Lhommels <arnaud.lhomme@epitech.eu>
2015-05-27 19:06:49 +0200

    début d'ia sur le client

--- src/client/python/connect.py
+++ src/client/python/connect.py
@@ -3,17 +3,17 @@
 
 import sys
 import socket
-import time
 
-print (sys.argv[1])
-print (sys.argv[2])
-print (sys.argv[3])
-print (sys.argv[4])
-if len(sys.argv) > 5:
- print (sys.argv[5])
-if len(sys.argv) > 6:
- print (sys.argv[6])
+#print (sys.argv[1])
+#print (sys.argv[2])
+#print (sys.argv[3])
+#print (sys.argv[4])
+#if len(sys.argv) > 5:
+# print (sys.argv[5])
+#if len(sys.argv) > 6:
+# print (sys.argv[6])
 
+team = sys.argv[2] + "\n"
 host = "localhost"
 if len(sys.argv) > 6:
  host = sys.argv[6]
@@ -22,5 +22,23 @@ port = int(sys.argv[4])
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 
+msg = s.recv(1024).decode()
+print (msg)
+s.send(team.encode())
+num_client = s.recv(1024).decode()
+print (num_client)
+position = s.recv(1024).decode()
+print (position)
+
+msg_send = ""
+while 1:
+        msg_send = input("> ")
+        if msg_send == "fin":
+         break
+        msg_send += "\n"
+        msg_send = msg_send.encode()
+        s.send(msg_send)
+        msg_recv = s.recv(1024).decode()
+        print (msg_recv)
 
 s.close()

33b973b120916085aaabb408fa257a2332c81c03
lopez_t <thibaut.lopez@epitech.eu>
2015-05-27 11:44:38 +0200

    fork ok

--- src/client/my_fork.c
+++ src/client/my_fork.c
@@ -5,7 +5,7 @@
 ** Login   <lopez_t@epitech.net>
 ** 
 ** Started on  Tue May 12 14:56:11 2015 Thibaut Lopez
-** Last update Wed May 27 11:23:55 2015 Thibaut Lopez
+** Last update Wed May 27 11:43:50 2015 Thibaut Lopez
 */
 
 #include "server.h"
@@ -23,6 +23,10 @@ int	my_fork(char **com, t_zap *data, t_user *usr)
   egg.pos.s = GET_Y(usr);
   egg.hatch.tv_sec = now.tv_sec;
   egg.hatch.tv_usec = now.tv_usec;
+  add_tv(&egg.hatch, 300000000 / data->delay);
   push_q(&GET_TEAM(usr)->eggs, &egg, clone_egg);
+  GET_TEAM(usr)->count++;
+  fill_cb(&usr->wr, "ok\n", 3);
+  push_q(&usr->queue, &now, clone_tv);
   return (0);
 }

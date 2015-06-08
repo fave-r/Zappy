c180967bfcc851a91b6404ea5d9fb96f5d66580e
Romaric FAVE <romaric.fave@epitech.eu>
2015-06-08 13:03:10 +0200

    init

---
+++ clients/graphic/src/main.cpp
@@ -0,0 +1,14 @@
+//
+// main.cpp for zappy in /home/fave_r/rendu/Zappy/clients/graphic/src
+//
+// Made by romaric
+// Login   <fave_r@epitech.net>
+//
+// Started on  Mon Jun  8 13:02:03 2015 romaric
+// Last update Mon Jun  8 13:02:33 2015 romaric
+//
+
+int	main(int ac, char **av)
+{
+  return (0);
+}

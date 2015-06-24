02457cb296755a4dd650bb65d56d0ab937d35934
ClementJean48 <clement.jean@epitech.eu>
2015-06-24 17:17:20 +0200

    Texture Selected ground

--- clients/graphic/Includes/TexturePool.hh
+++ clients/graphic/Includes/TexturePool.hh
@@ -5,7 +5,7 @@
 // Login   <jean_c@epitech.net>
 //
 // Started on  Sun Jun 21 02:31:35 2015 jean_c
-// Last update Sun Jun 21 02:37:09 2015 jean_c
+// Last update Wed Jun 24 17:07:02 2015 jean_c
 //
 
 #ifndef __TEXTUREPOOL_HH__
@@ -29,6 +29,7 @@ public:
 
 private:
   gdl::Texture		_textureGround;
+  gdl::Texture		_textureSelectedGround;
 
 };
 
--- clients/graphic/Sources/TexturePool.cpp
+++ clients/graphic/Sources/TexturePool.cpp
@@ -5,7 +5,7 @@
 // Login   <jean_c@epitech.net>
 //
 // Started on  Sun Jun 21 02:33:58 2015 jean_c
-// Last update Wed Jun 24 09:44:17 2015 jean_c
+// Last update Wed Jun 24 17:08:01 2015 jean_c
 //
 
 #include "TexturePool.hh"
@@ -22,6 +22,8 @@ void                    TexturePool::init()
 {
   if (this->_textureGround.load("./Ressources/Textures/ground.tga", true) == false)
     throw loading_error("TexturePool : Cannot load Ground texture");
+  else if (this->_textureSelectedGround.load("./Ressources/Textures/selectedGround.tga", true) == false)
+    throw loading_error("TexturePool : Cannot load Selected Ground texture");
 }
 
 const gdl::Texture      &TexturePool::getGround()

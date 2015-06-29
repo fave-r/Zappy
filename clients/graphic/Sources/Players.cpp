ce91acd84e99f9d6941769d68d32e9442e2dd794
Romaric FAVE <romaric.fave@epitech.eu>
2015-06-29 12:32:59 +0200

    p

---
+++ clients/graphic/Sources/Players.cpp
@@ -0,0 +1,28 @@
+//
+// Players.cpp for zappy in /home/fave_r/rendu/Zappy/clients/graphic
+//
+// Made by romaric
+// Login   <fave_r@epitech.net>
+//
+// Started on  Mon Jun 29 12:15:56 2015 romaric
+// Last update Mon Jun 29 12:24:51 2015 romaric
+//
+
+#include "Players.hh"
+
+Players::Players(const int nb)
+{
+  this->_nb = nb;
+  this->_food = 0;
+  this->_linemate = 0;
+  this->_deraumere = 0;
+  this->_sibur = 0;
+  this->_mendiane = 0;
+  this->_phiras = 0;
+  this->_thystame = 0;
+}
+
+Players::~Players()
+{
+}
+

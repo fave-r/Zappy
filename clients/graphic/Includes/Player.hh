022cca5e9880795a77c556e5f7a8f0e9efe363ff
Romaric FAVE <romaric.fave@epitech.eu>
2015-07-01 10:44:51 +0200

    get play

--- clients/graphic/Includes/Player.hh
+++ clients/graphic/Includes/Player.hh
@@ -5,7 +5,7 @@
 // Login   <fave_r@epitech.net>
 //
 // Started on  Mon Jun 29 12:15:40 2015 romaric
-// Last update Mon Jun 29 12:52:57 2015 romaric
+// Last update Wed Jul  1 10:44:38 2015 romaric
 //
 
 #ifndef	PLAYER_HH
@@ -17,7 +17,7 @@
 class	Player
 {
 private:
-  int	        _nb;
+  int	      _nb;
   int         _x;
   int         _y;
   int         _dir;
@@ -34,6 +34,72 @@ private:
 public:
   Player(const int nb, const int x, const int y, const int dir, const int lev, const std::string &tn);
   ~Player();
+
+public:
+  int	getNb(void) const
+  {
+    return _nb;
+  };
+
+  int	getX(void) const
+  {
+    return _x;
+  };
+
+  int	getY(void) const
+  {
+    return _y;
+  };
+
+  int	getDir(void) const
+  {
+    return _dir;
+  };
+
+  int	getLevel(void) const
+  {
+    return _level;
+  };
+
+  std::string	getTn(void) const
+  {
+    return _tn;
+  };
+
+  int	getFood(void) const
+  {
+    return _food;
+  }
+
+  int	getLinemate(void) const
+  {
+    return _linemate;
+  };
+
+  int	getDeraumere(void) const
+  {
+    return _deraumere;
+  };
+
+  int	getSibur(void) const
+  {
+    return _sibur;
+  };
+
+  int	getMendiane(void) const
+  {
+    return _mendiane;
+  };
+
+  int	getPhiras(void) const
+  {
+    return _phiras;
+  };
+
+  int	getThystame(void) const
+  {
+    return _thystame;
+  };
 };
 
 #endif
